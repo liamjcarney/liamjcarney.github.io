ec8b7a6e684f571a57454209ba96c053a4de8107
NotNeelPatel <91706742+NotNeelPatel@users.noreply.github.com>
2024-05-01 15:42:25 -0400

    prototype for popup for featured work

---
+++ assets/featured.js
@@ -0,0 +1,9 @@
+function popUp(element){
+    const ent = document.getElementById(element);
+    ent.style.visibility = "visible";
+}
+
+function popDown(element){
+    const ent = document.getElementById(element);
+    ent.style.visibility = "hidden";
+}
