a307a19cf52e46e0c2c50ab339fcf1c9624e7357
Neel Patel <91706742+NotNeelPatel@users.noreply.github.com>
2024-08-15 22:40:11 -0400

    added dynamic resizing for album showcases

--- assets/scripts/featured.js
+++ assets/scripts/featured.js
@@ -4,29 +4,46 @@ var discography = []
 
 // for discography.json sed "s/\"/'/g"
 
+// Stores current width in index 0 and previous width when popUp was last called
+var windowWidth = 0;
+var currentIndex = 0;
+
+const showcaseContainers = [
+    "",
+    "",
+    "-mobile"
+]
+
+const getWidth = () => window.innerWidth > 600 ? 1 : 2;
+
+
 
 function popUp(idx, close = true){
-    const ent = document.getElementById("album-showcase-container");
+    windowWidth = getWidth();
+    currentIndex = idx;
+    let w = showcaseContainers[windowWidth]
+    const ent = document.getElementById("album-showcase-container" + w);
     if(ent.style.visibility == "visible" && close) {
         ent.style.opacity = "0"
         ent.style.visibility = "hidden";
         document.getElementById("album-showcase-embed").innerHTML = "";
         document.body.classList.remove("remove-scrolling");
+        windowWidth = 0
         return;
     }
         let entry = discography[idx]
         document.body.classList.add("remove-scrolling");
-        document.getElementById("album-showcase-name").innerText=entry.name;
-        document.getElementById("album-showcase-title").innerText=entry.title;
-        document.getElementById("album-showcase-description").innerText=entry.description;
-        document.getElementById("album-showcase-cover").src = entry.img_location;
-        document.getElementById("album-showcase-embed").innerHTML = entry.embed;
+        document.getElementById("album-showcase-name" + w).innerText=entry.name;
+        document.getElementById("album-showcase-title" + w).innerText=entry.title;
+        document.getElementById("album-showcase-description" + w).innerText=entry.description;
+        document.getElementById("album-showcase-cover" + w).src = entry.img_location;
+        document.getElementById("album-showcase-embed" + w).innerHTML = entry.embed;
         
-        let next = (idx + 1 == discography.length) ? 0 : idx + 1
-        let prev = (idx - 1 == -1)? discography.length - 1 : idx - 1
+        let next = (idx + 1 == discography.length) ? 0 : idx + 1;
+        let prev = (idx - 1 == -1)? discography.length - 1 : idx - 1;
     
-        document.getElementById("button-left").onclick = function() { nextShowcase(prev) }
-        document.getElementById("button-right").onclick = function() { nextShowcase(next) }
+        document.getElementById("button-left"+ w).onclick = function() { nextShowcase(prev) }
+        document.getElementById("button-right"+ w).onclick = function() { nextShowcase(next) }
         ent.style.visibility = "visible";
 
         ent.style.opacity = "1"
@@ -56,3 +73,17 @@ fetch("./assets/scripts/discography.json")
         })
     })
 
+window.addEventListener("resize", function () {
+    if (windowWidth === 0) {
+        return;
+    }
+    let width = getWidth()
+    if (width !== windowWidth){
+        const oldEnt = document.getElementById("album-showcase-container" + showcaseContainers[windowWidth])
+        oldEnt.style.opacity = "0"
+        oldEnt.style.visibility = "hidden";
+        windowWidth = width;
+        popUp(currentIndex, false)
+        return;
+    }
+});
